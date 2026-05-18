5a21e852fb3415de050d85ab92e8a082dd06f168
Kartikey Gupta <kartikey.2004@icloud.com>
2026-05-18 12:20:03 +0530

    modified:   src/config/settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,16 +5,12 @@ load_dotenv()
 
 
 class Settings:
-
-    # API KEYS
-
+    
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-    # ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
     OPENAI_MODEL = "gpt-4o-mini"
     GEMINI_MODEL = "gemini-2.5-flash"
-    # CLAUDE_MODEL = "claude-sonnet-4-20250514"
 
     TEMPERATURE = 0.2
     MAX_RETRIES = 2
