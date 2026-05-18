1bcaa7176eb56ff557762ef3ae5d80fe77d22a81
Kartikey Gupta <kartikey.2004@icloud.com>
2026-05-18 12:19:15 +0530

    modified:   .env.example 	deleted:    src/config.py 	modified:   src/config/settings.py 	deleted:    src/llms.py 	renamed:    src/config/models.py -> src/llms/models.py 	new file:   src/llms/router.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,12 +3,22 @@ import os
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+class Settings:
+
+    # API KEYS
+
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+    # ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+
+    OPENAI_MODEL = "gpt-4o-mini"
+    GEMINI_MODEL = "gemini-2.5-flash"
+    # CLAUDE_MODEL = "claude-sonnet-4-20250514"
+
+    TEMPERATURE = 0.2
+    MAX_RETRIES = 2
+    DEBUG = True
 
 
-OPENAI_MODEL = "gpt-4o-mini"
-GEMINI_MODEL = "gemini-2.5-flash"
-
-TEMPERATURE = 0.2
-MAX_TOKENS = 2048
+settings = Settings()
